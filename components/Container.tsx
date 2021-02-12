a79a085111c65edfb0e4ea7c64d10d384139e63b
DmitryScaletta <dmitryscaletta@mail.ru>
2021-02-12 11:18:58 +0300

    feat: add github button

--- components/Container.tsx
+++ components/Container.tsx
@@ -19,6 +19,15 @@ const ArrowNext = (props) => (
   </svg>
 );
 
+const GithubIcon = (props) => (
+  <svg viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"
+    ></path>
+  </svg>
+);
+
 const ContainerRoot = styled.div`
   position: relative;
   padding-bottom: 128px;
@@ -65,6 +74,24 @@ const StyledArrowPrev = styled(ArrowPrev)`
 const StyledArrowNext = styled(ArrowNext)`
   ${articleIcon};
 `;
+const GithubButton = styled.a`
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  padding: 4px;
+  justify-content: flex-end;
+  height: 56px;
+  width: 56px;
+  color: var(--color-gray);
+  background-color: #171717;
+  clip-path: polygon(0 0, 100% 0, 100% 100%);
+
+  svg {
+    height: 24px;
+    width: 24px;
+  }
+`;
 
 type Props = {
   children: React.ReactNode;
@@ -105,6 +132,14 @@ const Container = ({
           </NextArticle>
         </Link>
       )}
+      <GithubButton
+        href="https://github.com/honeykingdom/hpg-newspaper"
+        target="_blank"
+        rel="noreferrer noopener"
+        title="Github Repository"
+      >
+        <GithubIcon />
+      </GithubButton>
     </ContainerRoot>
   );
 };
