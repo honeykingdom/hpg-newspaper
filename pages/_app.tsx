7eb526f42c0ea91b968bc89a1c32034d739d382b
DmitryScaletta <dmitryscaletta@mail.ru>
2021-02-12 15:37:44 +0300

    feat: add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,7 +36,24 @@ const App = ({ Component, pageProps }) => {
           href="https://fonts.googleapis.com/css2?family=Chakra+Petch&family=Inter:wght@500;800&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
