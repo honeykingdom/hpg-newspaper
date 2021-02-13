a4fb7445b6b03be2ec96a8beb51d664e905d3077
DmitryScaletta <dmitryscaletta@mail.ru>
2021-02-13 10:29:44 +0300

    chore: remove mdx plugin

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-});
-
-module.exports = withMDX({
+module.exports = {
   webpack: (config) => {
     // make @mdx-js/runtime work correctly
     config.node.fs = "empty";
 
     return config;
   },
-});
+};
