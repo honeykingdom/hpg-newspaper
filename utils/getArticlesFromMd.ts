9b74676f3f68f3e6ffe53068b49e30335210aa22
DmitryScaletta <dmitryscaletta@mail.ru>
2021-02-13 17:04:38 +0300

    fix: update md parser

--- utils/getArticlesFromMd.ts
+++ utils/getArticlesFromMd.ts
@@ -1,10 +1,10 @@
 import { promises as fs } from "fs";
 import assert from "assert";
 import yaml from "js-yaml";
-import { StatisticItem } from "../components/HpgLayout";
+import type { StatisticItem } from "../types";
 
-const METADATA_REGEX = /<!--(.+)-->/s;
-const CONTENT_REGEX = /^.+-->(.+)$/s;
+const METADATA_REGEX = /```yaml(.+)```/s;
+const CONTENT_REGEX = /```yaml.+```(.+)/s;
 
 const getRawMetadata = (content: string) => {
   const m = METADATA_REGEX.exec(content);
@@ -18,6 +18,29 @@ const parseYaml = (content: string) => {
   return yaml.load(content.replace(/\t/g, "  "));
 };
 
+const parseMetadata = (rawMetadata: string) => {
+  const parts = rawMetadata.split(/(```)/);
+
+  assert(/^# Выпуск \d+/.test(parts[0]));
+
+  const number = parts[0].slice(9).trim();
+
+  assert(parts[2].startsWith("yaml"));
+  assert(parts[6].startsWith("jsx"));
+
+  const metadata = parseYaml(parts[2].slice(4)) as {
+    date: string;
+    map: string;
+    statistics: StatisticItem[];
+  };
+  const layout = parts[6].slice(3).trim();
+
+  assert(typeof metadata === "object");
+  assert(/^\d{2}\.\d{2}\.\d{4}$/.test(metadata.date));
+
+  return { number, ...metadata, layout };
+};
+
 const normalizeTitle = (title: string) =>
   title.trim() === "-" ? null : title.trim();
 
@@ -26,20 +49,7 @@ const getArticlesFromMd = async (filename) => {
 
   const [rawMetadata, ...rawArticles] = content.split("\n## ");
 
-  const lines = rawMetadata.split("\n");
-
-  assert(/^# Выпуск \d+/.test(lines[0]));
-
-  const number = lines[0].slice(9);
-  const metadata = parseYaml(getRawMetadata(rawMetadata));
-
-  assert(typeof metadata === "object");
-
-  const { date, map, statistics } = metadata as {
-    date: string;
-    map: string;
-    statistics: StatisticItem[];
-  };
+  const metadata = parseMetadata(rawMetadata);
 
   const articles = rawArticles.map((rawArticle) => {
     const lines = rawArticle.split("\n");
@@ -47,12 +57,12 @@ const getArticlesFromMd = async (filename) => {
     const title = normalizeTitle(lines[0]);
     const subtitleRaw = lines.find((line) => line.startsWith("### "));
 
-    assert(subtitleRaw);
+    assert(subtitleRaw, filename);
 
     const subtitle = normalizeTitle(subtitleRaw.slice(4));
     const metadata = parseYaml(getRawMetadata(rawArticle));
 
-    assert(typeof metadata === "object");
+    assert(typeof metadata === "object", filename);
 
     let content: null | string = null;
 
@@ -73,7 +83,7 @@ const getArticlesFromMd = async (filename) => {
     };
   });
 
-  return { date, map, number, statistics, articles };
+  return { ...metadata, articles };
 };
 
 export default getArticlesFromMd;
