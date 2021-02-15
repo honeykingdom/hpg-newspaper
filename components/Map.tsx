d7dff1af9a8f3f008dba45e734dcc81e4dfdf367
DmitryScaletta <dmitryscaletta@mail.ru>
2021-02-15 13:30:09 +0300

    fix: use clip-path for the map border

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,43 +1,33 @@
 import styled from "styled-components";
-import MapBorder from "../public/images/map-border.svg";
 
 const MapRoot = styled.div`
-  position: relative;
-  border: 16px solid #000;
-  clip-path: polygon(
-    calc(100% - 16px) 0,
-    100% 16px,
-    100% calc(100% - 16px),
-    calc(100% - 16px) 100%,
-    16px 100%,
-    0 calc(100% - 16px),
-    0 16px,
-    16px 0
-  );
-
-  @media (min-width: 864px) {
-    padding: 16px;
-    border: none;
-    clip-path: none;
-  }
-`;
-const StyledMapBorder = styled(MapBorder)`
-  display: none;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-
-  @media (min-width: 864px) {
-    display: block;
-  }
-`;
-const ImageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #1b1d1e;
+  border: 16px solid #000;
+  clip-path: polygon(
+    62% 0,
+    63% 4px,
+    86% 4px,
+    87% 0,
+    calc(100% - 16px) 0,
+    100% 16px,
+    100% calc(100% - 16px),
+    calc(100% - 16px) 100%,
+    89% 100%,
+    88% calc(100% - 4px),
+    13% calc(100% - 4px),
+    12% 100%,
+    16px 100%,
+    0 calc(100% - 16px),
+    0 51%,
+    4px 50%,
+    4px 14%,
+    0 13%,
+    0 16px,
+    16px 0
+  );
 
   @media (min-width: 864px) {
     width: 768px;
@@ -56,10 +46,7 @@ type Props = {
 
 const Map = ({ src }: Props) => (
   <MapRoot>
-    <ImageWrapper>
-      <Image src={src} alt="HPG map" />
-    </ImageWrapper>
-    <StyledMapBorder />
+    <Image src={src} alt="HPG map" />
   </MapRoot>
 );
 
