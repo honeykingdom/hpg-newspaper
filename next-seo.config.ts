70992d9b648615d278f263e08e4c60a3c02174fb
DmitryScaletta <dmitryscaletta@mail.ru>
2021-02-23 13:54:56 +0300

    🔧 add next seo config

---
+++ next-seo.config.ts
@@ -0,0 +1,12 @@
+import type { DefaultSeoProps } from "next-seo";
+
+const config: DefaultSeoProps = {
+  title: "HPG Newspaper",
+  description: "HPG Newspaper. Газета с последними новостями по ХПГ 3.0",
+  openGraph: {
+    type: "website",
+    locale: "ru_RU",
+  },
+};
+
+export default config;
