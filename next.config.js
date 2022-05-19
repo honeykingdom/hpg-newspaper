8013ce862dbc8de8ee11a4d23e88d3a6dd82c0c7
DmitryScaletta <dmitryscaletta@mail.ru>
2022-05-19 15:40:10 +0300

    🔧 Update next config

--- next.config.js
+++ next.config.js
@@ -2,20 +2,12 @@ const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   webpack: (config) => {
-    // make @mdx-js/runtime work correctly
-    config.node.fs = "empty";
-
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
 
     return config;
-  },
-  // TODO: remove after next.js will fix it
-  // https://github.com/vercel/next.js/discussions/19372#discussioncomment-135696
-  images: {
-    loader: "imgix",
   },
   pwa: {
     disable: process.env.NODE_ENV === "development",
